0524c0d3893de1839081969137253507f89ed76d
Jehdi Aizon <jaizon2000@gmail.com>
2020-12-26 18:36:08 -0700

    keep player inbound

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,17 +6,20 @@ public class PlayerController : MonoBehaviour
 {
     public float horizontalInput;
     public float speed = 10.0f;
-    
+
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
         horizontalInput = Input.GetAxis("Horizontal");
-        transform.Translate(Vector3.right * (horizontalInput * Time.deltaTime * speed));
+        transform.Translate(Vector3.right * (Time.deltaTime * horizontalInput * speed));
+        
+        // Makes it so that it's only visible in a given range
+        // Mathf.Clamp: https://docs.unity3d.com/ScriptReference/Mathf.Clamp.html
+        transform.position = new Vector3(Mathf.Clamp(transform.position.x, -10, 10), 0.0f, 0.0f);
     }
 }

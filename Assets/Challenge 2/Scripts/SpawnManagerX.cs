5f027c21e16d5cb9ab7be68e8c7fcf4e8b9e0fbf
Jehdi Aizon <jaizon2000@gmail.com>
2020-12-28 18:50:10 -0700

    spawn random balls

--- Assets/Challenge 2/Scripts/SpawnManagerX.cs
+++ Assets/Challenge 2/Scripts/SpawnManagerX.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SocialPlatforms;
 
 public class SpawnManagerX : MonoBehaviour
 {
@@ -20,13 +21,13 @@ public class SpawnManagerX : MonoBehaviour
     }
 
     // Spawn random ball at random x position at top of play area
-    void SpawnRandomBall ()
+    void SpawnRandomBall()
     {
         // Generate random ball index and random spawn position
         Vector3 spawnPos = new Vector3(Random.Range(spawnLimitXLeft, spawnLimitXRight), spawnPosY, 0);
+        GameObject randomBall = ballPrefabs[Random.Range(0, ballPrefabs.Length)];
 
         // instantiate ball at random spawn location
-        Instantiate(ballPrefabs[0], spawnPos, ballPrefabs[0].transform.rotation);
+        Instantiate(randomBall, spawnPos, randomBall.transform.rotation);
     }
-
 }

b0027a6a96cd6587e0fc6e3f6c7e8b8f47689442
Jehdi Aizon <jaizon2000@gmail.com>
2020-12-28 18:03:57 -0700

    Spawn the animals at timed intervals

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -7,21 +7,21 @@ public class SpawnManager : MonoBehaviour
     public GameObject[] animalPrefabs;
 
     private float spawnRangeX = 20;
-
     private float spawnRangeZ = 20;
+
+    private float startDelay = 2;
+    private float spawnInterval = 1.5f;
 
     // Start is called before the first frame update
     void Start()
     {
+        // Call SpawnRandomAnimal() starting at startDelay and repeat every spawnInterval
+        InvokeRepeating("SpawnRandomAnimal", startDelay, spawnInterval);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.S))
-        {
-            SpawnRandomAnimal();
-        }
     }
 
     void SpawnRandomAnimal()
